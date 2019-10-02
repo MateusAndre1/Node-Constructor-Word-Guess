44dfbf3cdf671748b04257c65a286c703f34d5da
Andre Roque <mateusandre1@yahoo.com>
2019-10-01 22:02:07 -0400

    almost done with main logic, finished logging info for user input

--- index.js
+++ index.js
@@ -59,5 +59,49 @@ var guessesLeft = 10;
 // create a function for the core logic
 
 function mainLogic() {
-    
+
+    // give a new word if user decides to keep playing
+    if (newWordNeeded) {
+        var randomMCU = Math.floor(Math.random() * mcUniverse.length);
+        var randomWord = mcUniverse[randomMCU];
+        pickedWord = new Word(randomWord);
+        // set new word back to false until user decides to keep playing
+        newWordNeeded = false;
+    }
+
+    // array to put the chosen word
+    let chosenWord = [];
+
+    // use inquirer to get input from user
+    if (chosenWord.includes(false)) {
+        inquirer.prompt([{
+            type: "input",
+            message: "Pick a letter from A to Z",
+            name: "userInput"
+        }]).then(function (input) {
+            // capture the user input and display corresponding logs
+            if (!azArray.includes(input.userInput) || input.userInput.length > 1) {
+                console.log(`\nHave to enter something useful..\n`);
+                mainLogic();
+            } else if (incorrectArr.includes(input.userInput) || correctArr.includes(input.userInput) || input.userInput === "") {
+                console.log(`\nThis has already been guessed\n`);
+                mainLogic();
+            } else {
+                // make an array to have user input pushed into it to check if it is correct
+                var checkWordArr = [];
+                pickedWord.userGuess(input.userInput);
+                pickedWord.letterArray.forEach(checkWord);
+                if (checkWordArr.join("") === chosenWord.join("")) {
+                    incorrectArr.push(input.userInput)
+                    guessesLeft--;
+                    console.log(`\nIncorrect\n`);
+                } else {
+                    correctArr.push(input.userInput);
+                    console.log(`\nAtta boy! You got it one!\n`);
+                }
+            }
+        });
+    } else {
+        console.log(`YOU WIN!\n`);
+    }
 }
