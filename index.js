d997047273004be4e4a8cb1d5c3204fb6f590e10
Andre Roque <mateusandre1@yahoo.com>
2019-10-01 20:26:24 -0400

    psuedo coding done for index

---
+++ index.js
@@ -0,0 +1,6 @@
+// call Word.js to select words and use the Word constructor to store the values
+// use inquirer to create the logic to have a user play the game
+// have users see they have 10 guesses
+// show many guesses remain
+// display guessed letters in the terminal
+
